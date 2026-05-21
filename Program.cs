f91379738befa53796e2f24c70a4359316d10184
VictoriaBuchko <123644516+VictoriaBuchko@users.noreply.github.com>
2026-05-21 20:38:41 +0300

    Remove non-English comments from Program.cs
    Removed comments in non-English language for clarity.

--- Program.cs
+++ Program.cs
@@ -4,15 +4,11 @@ using Currency.Services;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-// Реєструємо репозиторії
 builder.Services.AddScoped<IUserRepository, MockUserRepository>();
 builder.Services.AddScoped<ICurrencyRateRepository, MockCurrencyRateRepository>();
 builder.Services.AddScoped<IUserAlertRepository, MockUserAlertRepository>();
-
-// Реєструємо сервіси
 builder.Services.AddScoped<IEmailService, EmailService>();
 
-// Реєструємо фоновий сервіс
 builder.Services.AddHostedService<CurrencyMonitoringService>();
 
 var host = builder.Build();
